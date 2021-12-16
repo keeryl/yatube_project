29d568a844daf436641a134a0c86a8d750eb32fa
Кирилл Зикевский <2309696@mail.ru>
2021-12-16 21:38:05 +0300

    dev: Добавлены urls и view-функции

---
+++ yatube/posts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+
+# Страница со списком мороженого
+def group_posts(request, slug):
+    return HttpResponse(f'Публикации группы {slug}')
